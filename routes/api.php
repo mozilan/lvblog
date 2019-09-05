e92de7a8da46c470c048738402e7cefd9ecda288
mozilan <mozilan.aliyun.com>
2019-09-05 13:59:35 +0000

    user register using phone

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,9 @@ $api->version('v1', [
     // 短信验证码
     $api->post('verificationCodes', 'VerificationCodesController@store')
         ->name('api.verificationCodes.store');
+    // 用户注册
+    $api->post('users', 'UsersController@store')
+        ->name('api.users.store');
 });
 $api->version('v2', function($api) {
     $api->get('version', function() {
