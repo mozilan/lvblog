3b458d62f0266cc5b0dfd3a578bb53275df8ab6f
mozilan <mozilan.aliyun.com>
2019-09-05 14:25:56 +0000

    add captcha

--- app/Http/Controllers/API/VerificationCodesController.php
+++ app/Http/Controllers/API/VerificationCodesController.php
@@ -10,13 +10,19 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
-        $phone = $request->phone;
+        $captchaData = \Cache::get($request->captcha_key);
 
-        // 生成4位随机数，左侧补0
-        $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+        if (!$captchaData) {
+            return $this->response->error('图片验证码已失效', 422);
+        }
 
-        $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
 
+        $phone = $captchaData['phone'];
         if (!app()->environment('production')) {
             $code = '1234';
         }else {
@@ -35,6 +41,9 @@ class VerificationCodesController extends Controller
         $expiredAt = now()->addMinutes(10);
         // 缓存验证码 10分钟过期。
         \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        // 清除图片验证码缓存
+        \Cache::forget($request->captcha_key);
 
         return $this->response->array([
             'key' => $key,
--- routes/api.php
+++ routes/api.php
@@ -35,6 +35,9 @@ $api->version('v1', [
         // 用户注册
         $api->post('users', 'UsersController@store')
             ->name('api.users.store');
+        // 图片验证码
+        $api->post('captchas', 'CaptchasController@store')
+            ->name('api.captchas.store');
     });
 });
 $api->version('v2', function($api) {
