257f86a99356cdf55fa6f4f12572d35681ea1977
mozilan <mozilan.aliyun.com>
2019-09-05 14:20:25 +0000

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -21,14 +21,21 @@ $api = app('Dingo\Api\Routing\Router');
 //    });
 //});
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
+    'namespace' => 'App\Http\Controllers\Api',
 ], function($api) {
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
-    // 用户注册
-    $api->post('users', 'UsersController@store')
-        ->name('api.users.store');
+
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => 1,
+        'expires' => 1,
+    ], function($api) {
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+    });
 });
 $api->version('v2', function($api) {
     $api->get('version', function() {
