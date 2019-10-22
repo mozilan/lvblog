4d48f2415de2b02dc476e7cfab558ce27584a03d
mozilan <mozilan@aliyun.com>
2019-10-22 16:35:35 +0800

    production url

--- resources/js/config.js
+++ resources/js/config.js
@@ -6,16 +6,14 @@ var api_url = '';
 
 switch( process.env.NODE_ENV ){
     case 'development':
-        url = 'http://www.mozilan.com';
-        api_url = 'http://www.mozilan.com/api';
+        url = 'https://www.mozilan.com';
+        api_url = 'https://www.mozilan.com/api';
         break;
     case 'production':
         url = 'https://www.mozilan.com';
         api_url = 'https://www.mozilan.com/api';
         break;
 }
-var ValineAppID='vEWC8aDW7kv3G3fW021C6LII-gzGzoHsz';
-var ValineAppKey='mgzfz3n69093F8zHv6Q8EySf';
 export const LVBLOG_CONFIG = {
     URL:url,
     API_URL: api_url,
